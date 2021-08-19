3865507635ae45b9c5729c2aeb3d0301dcad2c3f
Kevin Robbins <kevin.robbins99@gmail.com>
2021-08-19 17:14:25 -0500

    Add initial cards app and models

---
+++ yugioh_opener/apps/cards/models.py
@@ -0,0 +1,45 @@
+from django.db import models
+from django.core.validators import MaxValueValidator
+
+
+class CardSet(models.Model):
+    full_name = models.CharField(max_length=128)
+    abbr = models.CharField(max_length=4)
+
+
+class CardAttribute(models.Model):
+    name = models.CharField(max_length=32)
+
+
+class CardType(models.Model):
+    name = models.CharField(max_length=32)
+
+
+class Card(models.Model):
+    name = models.CharField(max_length=128, unique=True)
+    attribute = models.ForeignKey(to=CardAttribute, on_delete=models.CASCADE)
+    type = models.ForeignKey(to=CardType, on_delete=models.CASCADE)
+    level = models.PositiveSmallIntegerField(validators=[MaxValueValidator(12)])
+    text = models.TextField(max_length=1024)
+    attack = models.PositiveSmallIntegerField()
+    defense = models.PositiveSmallIntegerField()
+    releases = models.ManyToManyField(to=CardSet, through='Release', related_name='cards')
+
+
+class Release(models.Model):
+    class Rarity(models.TextChoices):
+        COMMON = 'COMMON', 'Common'
+        RARE = 'RARE', 'Rare'
+        SUPER_RARE = 'SUPER_RARE', 'Super Rare'
+        ULTRA_RARE = 'ULTRA_RARE', 'Ultra Rare'
+        SECRET_RARE = 'SECRET_RARE', 'Secret Rare'
+
+    set = models.ForeignKey(to=CardSet, related_name='sets', on_delete=models.CASCADE)
+    card = models.ForeignKey(to=Card, related_name='cards', on_delete=models.CASCADE)
+    card_number = models.PositiveSmallIntegerField(validators=[MaxValueValidator(999)])
+    rarity = models.CharField(choices=Rarity.choices, default=Rarity.COMMON, max_length=32)
+
+    class Meta: 
+        constraints = [
+            models.UniqueConstraint(fields=['set_id', 'card_id'], name='unique card/set')
+        ]
--- yugioh_opener/yugioh_opener/settings.py
+++ yugioh_opener/yugioh_opener/settings.py
@@ -12,10 +12,11 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import getpass
 from pathlib import Path
+import sys
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
+sys.path.append(str(BASE_DIR / 'apps'))
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
@@ -38,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'cards',
 ]
 
 MIDDLEWARE = [
